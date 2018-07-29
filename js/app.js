69ea0412ed2f4a386cf65b013821ae6ec88db658
Jean <jean.soro007@gmail.com>
2018-07-29 11:45:34 -0400

    Updates

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const menuBtn = document.getElementsByClassName('hamburger-menu')
+const mobileMenu = document.getElementsByClassName('mobile-menu')
+
+let clickedBtn = function() {
+    mobileMenu[0].classList.toggle('active')
+}
+
+
+menuBtn[0].addEventListener('click',clickedBtn)
+
