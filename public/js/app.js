7383e43627cfa1e5bb7e3d75cc3813c576784341
Jean <jean.soro007@gmail.com>
2019-10-08 20:27:25 -0400

    slight revamping of ui

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,8 @@
 const menuBtn = document.getElementsByClassName('hamburger-menu')
 const mobileMenu = document.getElementsByClassName('mobile-menu')
 
-let clickedBtn = function() {
-    mobileMenu[0].classList.toggle('active')
-}
+let clickedBtn = () => mobileMenu[0].classList.toggle('active')
 
+menuBtn[0].addEventListener('click', clickedBtn)
 
-menuBtn[0].addEventListener('click',clickedBtn)
-
+document.querySelector('')
