dd700e345e341d651938ffb6c8c693e89728a185
Jean <jean.soro007@gmail.com>
2020-05-21 18:56:52 -0400

    Added dark mode theme functionality, styles and button

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,27 @@
-const menuBtn = document.getElementsByClassName('hamburger-menu')
-const mobileMenu = document.getElementsByClassName('mobile-menu')
+const menuBtn = document.getElementsByClassName('hamburger-menu');
+const mobileMenu = document.getElementsByClassName('mobile-menu');
+const darkModeBtn = document.querySelector('.darkToggle');
+let body = document.body;
+let currentTheme = localStorage.getItem('currentTheme');
 
-let clickedBtn = () => mobileMenu[0].classList.toggle('active')
 
-menuBtn[0].addEventListener('click', clickedBtn)
+let clickedBtn = () => mobileMenu[0].classList.toggle('active');
+menuBtn[0].addEventListener('click', clickedBtn);
 
-document.querySelector('')
+if (currentTheme) {
+  body.classList.add('dark-theme');
+}
+
+
+darkModeBtn.addEventListener('click', () => {
+  body.classList.toggle('dark-theme');
+
+  if (body.classList.contains('dark-theme')) {
+    localStorage.setItem('currentTheme', 'themeActive')
+  } else {
+    localStorage.removeItem('currentTheme');
+  }
+});
+
+// document.querySelector('')
+
