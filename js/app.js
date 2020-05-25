6abe61598c9ecc813971d1c2f3b20f86041dc030
Jean <jean.soro007@gmail.com>
2020-05-24 23:35:29 -0400

    Updates

--- js/app.js
+++ js/app.js
@@ -1,8 +1,41 @@
-const menuBtn = document.getElementsByClassName('hamburger-menu')
-const mobileMenu = document.getElementsByClassName('mobile-menu')
+const menuBtn = document.getElementsByClassName('hamburger-menu');
+const mobileMenu = document.getElementsByClassName('mobile-menu');
+const darkModeBtn = document.querySelector('.darkToggle');
+let body = document.body;
+let currentTheme = localStorage.getItem('currentTheme');
+let backToTopBtn = document.querySelector('.backToTopBtn');
 
-let clickedBtn = () => mobileMenu[0].classList.toggle('active')
 
-menuBtn[0].addEventListener('click', clickedBtn)
+let clickedBtn = () => mobileMenu[0].classList.toggle('active');
+menuBtn[0].addEventListener('click', clickedBtn);
 
-document.querySelector('')
+
+
+//Dark Mode
+if (currentTheme) {
+  body.classList.add('dark-theme');
+}
+
+darkModeBtn.addEventListener('click', () => {
+  body.classList.toggle('dark-theme');
+
+  if (body.classList.contains('dark-theme')) {
+    localStorage.setItem('currentTheme', 'themeActive')
+  } else {
+    localStorage.removeItem('currentTheme');
+  }
+});
+
+//back to top
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+}
+
