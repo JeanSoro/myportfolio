3367d427f72ce5a5a548f2e832e9da73da0ebdfd
Jean <jean.soro007@gmail.com>
2020-05-24 23:28:17 -0400

    Added back to top button and edited dark mode styles

--- public/js/app.js
+++ public/js/app.js
@@ -3,15 +3,18 @@ const mobileMenu = document.getElementsByClassName('mobile-menu');
 const darkModeBtn = document.querySelector('.darkToggle');
 let body = document.body;
 let currentTheme = localStorage.getItem('currentTheme');
+let backToTopBtn = document.querySelector('.backToTopBtn');
 
 
 let clickedBtn = () => mobileMenu[0].classList.toggle('active');
 menuBtn[0].addEventListener('click', clickedBtn);
 
+
+
+//Dark Mode
 if (currentTheme) {
   body.classList.add('dark-theme');
 }
-
 
 darkModeBtn.addEventListener('click', () => {
   body.classList.toggle('dark-theme');
@@ -23,5 +26,16 @@ darkModeBtn.addEventListener('click', () => {
   }
 });
 
-// document.querySelector('')
+//back to top
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+}
 
